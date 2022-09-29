e1f58af80acb0c30813fbec2ac539c983cb46628
Nicolas Rueda <nicolasthrash25@hotmail.com>
2022-09-29 12:52:45 -0500

    version 0.0 terminada y se logro implementar RK4 para un sistema de 4 ecus acoplada. Version 1.0 terminada, la en la siguiente version se espera poder imprimir 2 archivos de texto el primero con los datos de intensidad el segundo con los datos necesarios para la seccion de Poncaire

--- RK4-JM0.0.cpp
+++ RK4-JM0.0.cpp
@@ -1,92 +1,130 @@
-#include <cmath>
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <fstream>
+#include <vector>
 
+using alg=double (double, double, double, double, double);
 
-double h(double& a_r,double& a_i,double& sigma_i, double& t, const double phi, const double f, const double Omega, const double omega_a,const double g, const double k); //dar/dt
-double i(double& sigma_r,double& a_i,double& sigma_i, double& t,const double omega_sigma, const double g,const double gamma); //d(sigma_r)/dt
-double j(double& a_r,double& a_i,double& sigma_i, double& t, const double omega_a, const double g, const double k); // d(ai)/dt
-double k(double& a_r,double& a_i,double& sigma_i, double& t, const double omega_sigma, const double g, const double gamma); //d(sigma_i)/dt
-/*d(a_r)/dt = (\phi)(1+fcos(lambda)) + (omega_a)*a_i + g (sigma_i) - k/2(a_r)
-		  d(sigma_r)/dt = (omega_sigma)(sigma_i) - g(a_i)- 2g(a_i)(sigma_r)^2 + 2g(a_i)(sigma_i)^2 - (gamma)/2 (sigma_r)
-		  d(a_i)/dt = -(omega_a)(a_r) - g(sigma_r) - k/2 (a_i)
-		  d(sigma_i)/dt = -(omega_sigma)(sigma_i) + g(a_r) - 2g(a_r)(sigma_r)^2 - 2g(a_r)(sigma_i)^2 - (gamma)/2 (sigma_i)
-		  d(lambda)/dt = Omega => lambda=Omega*t
-*/
-void RK4(std::vector<double>& t, std::vector<double>& a_r, std::vector<double>& a_i, std::vector<double>& sigma_r,std::vector<double>& sigma_i,const double& h, const int& n); 
+double dif1(double x, double y, double z, double w, double t);
+double dif2(double x, double y, double z, double w, double t);
+double dif3(double x, double y, double z, double w, double t);
+double dif4(double x, double y, double z, double w, double t);
+double A(double t);
+void RK(double x0, double y0, double z0, double w0, double t0, int N);
 
-
-
-int main(int argc, char const *argv[])
-{
-	//Consts
-	double f=0;
-	double g=0;
-	double k=0;
-	double phi=0;
-	double gamma=0;
-	double Omega=0;
-	double omega_a=0;
-	double omega_sigma=0;
-
-		
-	int n= 100; //steps
-	double h=M_PI/5000; //step size
-	std::vector<double> t;
-	std::vector<double> a_r;
-	std::vector<double> a_i;
-	std::vector<double> sigma_r;
-	std::vector<double> sigma_i;
-
-	//ini cond
-	t[0]=0.0;
-	a_r[0]=4.0;
-	a_i[0]=4.0;
-	sigma_r[0]=0.2;
-	sigma_i[0]=0.2;
-	
+const double h=0.001;
+const double omega=1.01;
+const double phi=0.695;
+const double f=0.183;
+const double g=0.1965;
+const double k=0.07;
+const double gamma=0.0009;
+const double omega_a=1.0;
+const double omega_s=1.1;
 
 
 
 
-	return 0;
+
+int main(){
+    int N=1000000;
+    double x0=4.0;
+    double y0=0.8;
+    double z0=2.0;
+    double w0=0.4;
+    double t0=0;
+
+    RK(x0, y0, z0, w0, t0, N);
+    return 0;
 }
 
-
-double h(double& a_r,double& a_i,double& sigma_i, double& t, const double phi, const double f, const double Omega, const double omega_a,const double g, const double k){
-
-	return phi*(1.0+f*std::cos(Omega*t)) + omega_a*a_i + g*sigma_i - (k/2.0)*(a_r);
-}//ar
-
-double i(double& sigma_r,double& a_i,double& sigma_i, double& t,const double omega_sigma, const double g,const double gamma){
-
-	return omega_sigma*sigma_i - g*a_i + 2.0*g*a_i*std::pow(sigma_r,2.0) + (2.0)*g*a_i*std::pow(sigma_i,2.0) - (gamma/2.0)*(sigma_r);
-}//sigmar
-
-double j(double& a_r,double& a_i,double& sigma_i, double& t, const double omega_a, const double g, const double k){
-
-	return (-1.0)*omega_a*a_r - g*sigma_r - (k/2.0)*a_i;
-}//ai
-
-double k(double& a_r,double& a_i,double& sigma_i, double& t, const double omega_sigma, const double g, const double gamma){
-
-	return (-1.0)*omega_sigma*sigma_i + g*a_r - (2.0)*g*a_r*std::pow(sigma_r,2.0) - (2.0)*g*a_r*std::pow(sigma_i,2.0) - (gamma/2.0)*(sigma_i);
-}//sigmai
-
-void RK4(std::vector<double>& t, std::vector<double>& a_r, std::vector<double>& a_i, std::vector<double>& sigma_r,std::vector<double>& sigma_i,const double& h, const int& n){
-
-	double H1, H2, H3, H4, I1, I2, I3, I4, J1, J2, J3, J4, K1, K2, K3, K4=0;
-	for (int i = 0; i < n; ++i)
-	{
-		H1 = h(a_r[i],a_i[i],sigma_i[i],t[i],phi,f,Omega,omega_a,g,k);
-		I1 = i(sigma_r[i],a_i[i],sigma_i[i],t[i],omega_sigma,g,gamma);
-		J1 = j(a_r[i],a_i[i],sigma_i[i],t[i],omega_a,g,k);
-		K1 = k(a_r[i],a_i[i],sigma_i[i],t[i],omega_sigma,g,gamma);
-
-		H2 = h(a_r[i]+H1*(h/2.0),a_i[i]+J1*(h/2.0),sigma_i[i]+K1*(h/2.0),t[i]+(h/2.0),phi,f,Omega,omega_a,g,k);
-		I2 = i(sigma_r[i]+I1*(h/2.0),a_i[i]+J1*(h/2.0),sigma_i[i]+K1*(h/2.0),t[i]+(h/2.0),omega_sigma,g,gamma);
-		J2 = j(a_r[i]+H1*(h/2.0),a_i[i]+J1*(h/2.0),sigma_i[i]+K1*(h/2.0),t[i]+(h/2.0),omega_a,g,k);
-		K2 = k(a_r[i]+H1*(h/2.0),a_i[i]+J1*(h/2.0),sigma_i[i]+K1*(h/2.0),t[i]+(h/2.0),omega_sigma,g,gamma);
-
-		
-	}
+double dif1(double x, double y, double z, double w, double t){
+    return phi*(1+f*std::cos(A(t))+omega_a*z)+g*w-0.5*k*x;
+}
+double dif2(double x, double y, double z, double w, double t){
+    return omega_s*w-g*z+2*g*z*y*y+2*g*z*w*w-0.5*gamma*y;
+}
+double dif3(double x, double y, double z, double w, double t){
+    return -omega_a*x-g*y-0.5*k*z;
+}
+double dif4(double x, double y, double z, double w, double t){
+    return -omega_s*w+g*x-2*g*x*y*y-2*g*x*w*w-0.5*gamma*w;
+}
+double A(double t){
+    return omega*t;
 }
 
+void RK(double x0, double y0, double z0, double w0, double t0, int N){
+    double t=t0;
+    double x=x0;
+    double y=y0;
+    double z=z0;
+    double w=w0;
+    std::vector<double>data_x(N);
+    std::vector<double>data_y(N);
+    std::vector<double>data_z(N);
+    std::vector<double>data_w(N);
+    double k1, k2, k3, k4, l1, l2, l3, l4, m1, m2, m3, m4, n1, n2, n3, n4;
+        for(int i=0;i<N;i++){
+        k1=dif1(x,y,z,w,t);
+        l1=dif2(x,y,z,w,t);
+        m1=dif3(x,y,z,w,t);
+        n1=dif4(x,y,z,w,t);
+
+        k2=dif1(x+0.5*h*k1, y+0.5*h*l1, z+0.5*h*m1, w+0.5*h*n1, t+0.5*h);
+        l2=dif2(x+0.5*h*k1, y+0.5*h*l1, z+0.5*h*m1, w+0.5*h*n1, t+0.5*h);
+        m2=dif3(x+0.5*h*k1, y+0.5*h*l1, z+0.5*h*m1, w+0.5*h*n1, t+0.5*h);
+        n2=dif4(x+0.5*h*k1, y+0.5*h*l1, z+0.5*h*m1, w+0.5*h*n1, t+0.5*h);
+
+
+        k3=dif1(x+0.5*h*k2, y+0.5*h*l2, z+0.5*h*m2, w+0.5*h*n2, t+0.5*h);
+        l3=dif2(x+0.5*h*k2, y+0.5*h*l2, z+0.5*h*m2, w+0.5*h*n2, t+0.5*h);
+        m3=dif3(x+0.5*h*k2, y+0.5*h*l2, z+0.5*h*m2, w+0.5*h*n2, t+0.5*h);
+        n3=dif4(x+0.5*h*k2, y+0.5*h*l2, z+0.5*h*m2, w+0.5*h*n2, t+0.5*h);
+
+
+        k4=dif1(x+h*k3, y+h*l3, z+h*m3, w+h*n3, t+h);
+        l4=dif2(x+h*k3, y+h*l3, z+h*m3, w+h*n3, t+h);
+        m4=dif3(x+h*k3, y+h*l3, z+h*m3, w+h*n3, t+h);
+        n4=dif4(x+h*k3, y+h*l3, z+h*m3, w+h*n3, t+h);
+
+        data_x[i]=x;
+        data_y[i]=y;
+        data_z[i]=z;
+        data_w[i]=w;
+
+        t=t+h;
+
+        x=x+h*(k1+2*k2+2*k3+k4)/6;
+        y=y+h*(l1+2*l2+2*l3+l4)/6;
+        z=z+h*(m1+2*m2+2*m3+m4)/6;
+        w=w+h*(n1+2*n2+2*n3+n4)/6;
+    }
+    std::ofstream outfile;
+
+    outfile.open("data_x.txt");
+    for(int i=0; i<N; i++){
+        outfile<<data_x[i]<<std::endl;
+    }
+    outfile.close();
+
+    outfile.open("data_y.txt");
+    for(int i=0; i<N; i++){
+        outfile<<data_y[i]<<std::endl;
+    }
+    outfile.close();
+
+    outfile.open("data_z.txt");
+    for(int i=0; i<N; i++){
+        outfile<<data_z[i]<<std::endl;
+    }
+    outfile.close();
+
+    outfile.open("data_w.txt");
+    for(int i=0; i<N; i++){
+        outfile<<data_w[i]<<std::endl;
+    }
+    outfile.close();
+
+}
